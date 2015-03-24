12813b7ebcb1098a604f6e11fd3aecfd179d4837
Jasper Furniss <jasperfurniss@gmail.com>
2015-03-24 12:03:37 -0400

    account page edited

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -23,21 +23,4 @@ export default Ember.Object.extend({
      });
    },
 
-  //  uploadPicture: function(){
-  //    return ajax("https://api.parse.com/1/files/accountpicture.jpg").then(function(response){
-  //      return response.results.map(function(user) {
-  //        user.id = user.objectId;
-  //        delete user.objectId;
-  //        delete user.sessionToken;
-  //        return user;
-  //      });
-  //    });
-  //  }
-
-  //
-  // "X-Parse-Application-Id: 7yK2wek0IQahfvoyZVvzbfHH6SlsGzDNGW3PLSRQ" \
-  // "X-Parse-REST-API-Key: YHraWBoMYONsrzvgLxzTmFQc6bYk4Dj8gB06m5Hv" \
-  // "Content-Type: image/jpeg" \
-  // --data-binary '@myPicture.jpg' \
-  // https://api.parse.com/1/files/pic.jpg
 });
