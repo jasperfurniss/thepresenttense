6c1efd1ba8c72374ae297eadbff6b55bd1faf284
Jasper Furniss <jasperfurniss@gmail.com>
2015-03-25 22:20:54 -0400

    before autofocus plugin

--- app/controllers/goal.js
+++ app/controllers/goal.js
@@ -3,10 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
 
-    owner: function() {
-      return (this.session.isAuthenticated && this.get('session.content.currentUser.id') === this.get('model.goal.goalOwner.objectId'));
-    }.property(),
-
     destroy: function(goal) {
       goal.destroy();
     },
@@ -24,31 +20,6 @@ export default Ember.Controller.extend({
         this.transitionToRoute('goal');
       }.bind(this));
     },
-
-    // modal: function() {
-    //
-    //   var $;
-    //   $(function() {
-    //     $("#modal-1").on("change", function() {
-    //       if ($(this).is(":checked")) {
-    //         $("body").addClass("modal-open");
-    //       } else {
-    //         $("body").removeClass("modal-open");
-    //       }
-    //     });
-    //
-    //     $(".modal-fade-screen, .modal-close").on("click", function() {
-    //       $(".modal-state:checked").prop("checked", false).change();
-    //     });
-    //
-    //     $(".modal-inner").on("click", function(e) {
-    //       e.stopPropagation();
-    //     });
-    //   });
-    //
-    //
-    // }
-
   },
 
   isEditing: false,
